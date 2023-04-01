bafce8390470abc745ce1f564cd81a5aa3461a48
Luis Angel <luisparr14@gmail.com>
2023-04-01 15:53:04 -0500

    clase #19 - flutter avanzado

--- index.js
+++ index.js
@@ -4,21 +4,23 @@ const path = require('path')
 const { version } = require('./package.json')
 const app = express()
 
+app.set('port', process.env.PORT || 3000)
+
 const publicPath = path.resolve(__dirname, 'public')
 
 app.use(express.static(publicPath))
 
 app.use(logger('dev'))
 
-app.get('/health', (req, res) => {
+app.get('/health', (_, res) => {
   res.send('OK')
 })
 
-app.get('/version', (req, res) => {
+app.get('/version', (_, res) => {
   res.send(version)
 })
 
-app.listen(3000, (err) => {
+app.listen(app.get('port'), (err) => {
   if (err) return console.log('something bad happened', err)
-  console.log('Example app listening on port 3000!')
+  console.log(`Example app listening on port ${app.get('port')}`)
 })
