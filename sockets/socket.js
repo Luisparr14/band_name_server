6e9ac3840dd3d2b535bd6bf1416fefcb60c43246
Luis Angel <luisparr14@gmail.com>
2023-04-01 16:44:23 -0500

    clase #37 - flutter avanzado

--- sockets/socket.js
+++ sockets/socket.js
@@ -25,6 +25,17 @@ const socket = (io) => {
     })
 
     client.emit('active-bands', bands.getBands())
+
+    client.on('vote-band', (payload) => {
+      bands.voteBand(payload.id)
+      io.emit('active-bands', bands.getBands())
+    })
+
+    client.on('add-band', (payload) => {
+      const newBand = new Band(payload.name)
+      bands.addBand(newBand)
+      io.emit('active-bands', bands.getBands())
+    })
   })
 }
 
